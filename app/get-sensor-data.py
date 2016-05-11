145cdbe882bf54942aaae6e7c8fc4d6406462540
Brian Lin <brian.lin@dlinkcorp.com>
2016-05-11 10:00:39 +0800

    Adjust the pin number.

--- app/get-sensor-data.py
+++ app/get-sensor-data.py
@@ -3,38 +3,38 @@ import time
 import sqlite3
 
 
-# Define GPIO pin number.
-GPIO0_PIN = 11
-GPIO1_PIN = 12
-GPIO2_PIN = 13
-GPIO3_PIN = 15
-GPIO4_PIN = 16
-GPIO5_PIN = 18
-GPIO6_PIN = 22
-GPIO7_PIN = 7
+# GPIO_xx mapping to pin number.
+PIN7 = 7
+PIN11 = 11
+PIN12 = 12
+PIN13 = 13
+PIN15 = 15
+PIN16 = 16
+PIN18 = 18
+PIN22 = 22
 
 
 def setup_gpio():
     GPIO.setmode(GPIO.BOARD)
     # Set GPIO as input.
-    GPIO.setup(GPIO0_PIN, GPIO.IN)
-    GPIO.setup(GPIO1_PIN, GPIO.IN)
-    GPIO.setup(GPIO2_PIN, GPIO.IN)
-    GPIO.setup(GPIO3_PIN, GPIO.IN)
-    GPIO.setup(GPIO4_PIN, GPIO.IN)
-    GPIO.setup(GPIO5_PIN, GPIO.IN)
-    GPIO.setup(GPIO6_PIN, GPIO.IN)
-    GPIO.setup(GPIO7_PIN, GPIO.IN)
+    GPIO.setup(PIN7, GPIO.IN)
+    GPIO.setup(PIN11, GPIO.IN)
+    GPIO.setup(PIN12, GPIO.IN)
+    GPIO.setup(PIN13, GPIO.IN)
+    GPIO.setup(PIN15, GPIO.IN)
+    GPIO.setup(PIN16, GPIO.IN)
+    GPIO.setup(PIN18, GPIO.IN)
+    GPIO.setup(PIN22, GPIO.IN)
 
 def get_temp():
-    d0 = GPIO.input(GPIO0_PIN)
-    d1 = GPIO.input(GPIO1_PIN)
-    d2 = GPIO.input(GPIO2_PIN)
-    d3 = GPIO.input(GPIO3_PIN)
-    d4 = GPIO.input(GPIO4_PIN)
-    d5 = GPIO.input(GPIO5_PIN)
-    d6 = GPIO.input(GPIO6_PIN)
-    d7 = GPIO.input(GPIO7_PIN)
+    d0 = GPIO.input(PIN7)
+    d1 = GPIO.input(PIN11)
+    d2 = GPIO.input(PIN12)
+    d3 = GPIO.input(PIN13)
+    d4 = GPIO.input(PIN15)
+    d5 = GPIO.input(PIN16)
+    d6 = GPIO.input(PIN18)
+    d7 = GPIO.input(PIN22)
     value = (d7 << 7) + (d6 << 6) + (d5 << 5) + (d4 << 4) + (d3 << 3) + (d2 << 2) + (d1 << 1) + d0
     voltage = (value * 2.5 * 2) / 256
     temp = voltage * 1000 / 10
